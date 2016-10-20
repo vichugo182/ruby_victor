b76479c6ae5b5a659e44b67ccbecd685c364270a
Victor <vichugo182@gmail.com>
2016-10-20 14:13:02 -0300

    tarea1

---
+++ practica-1/case_work.rb
@@ -0,0 +1,13 @@
+variable = "false"
+otra_variable = "nil"
+
+case 
+when variable == "Algún texto"
+	puts "No es igual a Algún texto"
+when otra_variable == "Algún texto"
+  	puts "No es igual a Algún texto"
+when variable == false
+  	puts "Al parecer esto es lo que se debe ejecutar!"
+else
+	puts "Nada se ejecutó? Por que?"
+end
